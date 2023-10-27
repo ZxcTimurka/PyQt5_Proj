b05bab2e8c88e11d6db06501d3d25d1a202c8a7b
ZxcTimurka <130562086+ZxcTimurka@users.noreply.github.com>
2023-10-27 15:50:43 +0700

    Add files via upload

--- ai.py
+++ ai.py
@@ -1,23 +1,26 @@
-import cv2
-from PIL import Image
-from roboflow import Roboflow
-
-
 class Ai:
-    def __init__(self):
+    def __init__(self, file_path):
+        from roboflow import Roboflow
         rf = Roboflow(api_key="kDPogOFw5AbqDr95ytoZ")
         project = rf.workspace().project("license-plates-kwudy")
         self.model = project.version(5).model
+        self.file_path = file_path
 
-    def run(self, file_path):
-        image = Image.open(file_path)
-        result = self.model.predict(file_path, confidence=60, overlap=30).json()
-
+    def run(self):
+        result = self.model.predict(self.file_path, confidence=60, overlap=30).json()
+        self.coords = []
         for i in range(len(result['predictions'])):
             x1 = result['predictions'][i]['x'] - result['predictions'][i]['width'] // 2
             y1 = result['predictions'][i]['y'] - result['predictions'][i]['height'] // 2
             x2 = x1 + result['predictions'][i]['width']
             y2 = y1 + result['predictions'][i]['height']
 
-            cropped_image = image.crop((x1, y1, x2, y2))
+            self.coords.append([x1, y1, x2, y2])
+        print(f'{len(self.coords)} detected')
+
+    def show(self):
+        from PIL import Image
+        image = Image.open(self.file_path)
+        for i in self.coords:
+            cropped_image = image.crop(i)
             cropped_image.show()
