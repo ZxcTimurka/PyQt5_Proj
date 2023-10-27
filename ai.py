e6315cfd815ac03a563f272bbc520a84d3496883
ZxcTimurka <130562086+ZxcTimurka@users.noreply.github.com>
2023-10-27 18:56:59 +0700

    added func reader

--- ai.py
+++ ai.py
@@ -14,7 +14,7 @@ class LicensePlateDetector:
             y1 = prediction['y'] - prediction['height'] // 2
             x2 = x1 + prediction['width']
             y2 = y1 + prediction['height']
-            self.coords.append([x1, y1, x2, y2])
+            self.coords.append([round(x1), round(y1), round(x2), round(y2)])
         print(f'{len(self.coords)} detected')
 
     def show(self):
@@ -23,3 +23,21 @@ class LicensePlateDetector:
         for i in self.coords:
             cropped_image = image.crop(i)
             cropped_image.show()
+
+    def reader(self):
+        import easyocr
+        import cv2
+        img = cv2.imread(self.file_path)
+        reader = easyocr.Reader(['ru'])
+        for i in self.coords:
+            cropped_image = img[i[1]:i[3], i[0]:i[2]]
+            cv2.imshow('123', cropped_image)
+            gray = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+            cv2.imshow('123', gray)
+            print(reader.readtext(gray, detail=0))
+            cv2.waitKey(0)
+
+
+ai = LicensePlateDetector('imgs/n2.jpg')
+ai.run()
+ai.reader()
